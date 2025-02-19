5a20207e49d752bc92145ac4e5378599831b2c0c
Merllinyazhini <merllinyazhini005@gmail.com>
2025-02-19 10:48:06 +0530

    Initial commit

--- backend/models/quiz.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const QuizSchema = new mongoose.Schema({
-  question: { type: String, required: true },
-  options: { type: [String], required: true },
-  correctAnswer: { type: String, required: true }
-});
-
-module.exports = mongoose.model("Quiz", QuizSchema);
